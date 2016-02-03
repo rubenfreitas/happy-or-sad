9c89e58beab9fb28ad60cdf56dbbdaa3469a0c86
Rúben Freitas <ruben.madtech@gmail.com>
2016-02-03 23:16:57 +0000

    refactored client code, updated packages version, added tests samples

--- client/templates/textForm.js
+++ client/templates/textForm.js
@@ -3,19 +3,26 @@ Template.textForm.events = {
     // Don't postback
     e.preventDefault();
 
-    // create the new text
-    var newText = {
-      text: event.target.text.value,
-      isHappy: "unknown",
-      createdAt: new Date()
-    };
+    var phrase = event.target.text.value;
 
-    Meteor.call('happyOrSad', newText, function(err,response) {
-      if(err) {
-        console.log("Error:" + err.reason);
-		    return;
-			}
-		});
+    if (!!phrase && phrase.split(' ').length > 2) {
+      throw new Meteor.Error('text', 'You must enter a phrase with at least 3 words!');
+    } else {
+      // create the new text
+      var newText = {
+        text: phrase,
+        isHappy: "unknown",
+        createdAt: new Date()
+      };
+
+      Meteor.call('happyOrSad', newText, function(err,response) {
+        if(err) {
+          console.log("Error: " + err.reason);
+  		    return;
+  			}
+  		});
+
+    }
 
     // Clear form
     event.target.text.value = "";
